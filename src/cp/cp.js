d6f4dc51a2b55ff6fc20efa3c4ee3e3dc337fd38
z-e-a <e.a.zubkov@yandex.ru>
2024-10-02 19:24:18 +0300

    init: task 'Node.js basics'

---
+++ src/cp/cp.js
@@ -0,0 +1,6 @@
+const spawnChildProcess = async (args) => {
+    // Write your code here
+};
+
+// Put your arguments in function call to test this functionality
+spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
