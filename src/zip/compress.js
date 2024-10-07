fc589379ae17fb68e8ef5abeef8afe53c6ae1cb7
z-e-a <e.a.zubkov@yandex.ru>
2024-10-07 12:31:55 +0300

    feat: solve subtask 'Zlib'

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import { ReadStream, WriteStream } from "fs";
+import { pipeline } from "stream/promises";
+import path from "path";
+import zlib from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+  const toReadFilePath = path.resolve("src", "zip", "files", "fileToCompress.txt");
+  const readStream = new ReadStream(toReadFilePath);
+  const toWriteFilePath = path.resolve("src", "zip", "files", "archive.gz");
+  const writeStream = new WriteStream(toWriteFilePath);
+
+  await pipeline(
+  readStream,
+    zlib.createGzip(),
+  writeStream
+  );
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import { ReadStream, WriteStream } from "fs";
+import { pipeline } from "stream/promises";
+import path from "path";
+import zlib from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const toReadFilePath = path.resolve("src", "zip", "files", "archive.gz");
+  const readStream = new ReadStream(toReadFilePath);
+  const toWriteFilePath = path.resolve("src", "zip", "files", "fileToCompress.txt");
+  const writeStream = new WriteStream(toWriteFilePath);
+
+  await pipeline(
+  readStream,
+  zlib.createUnzip(),
+  writeStream
+  );
 };
 
 await decompress();
