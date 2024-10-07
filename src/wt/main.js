7f8aca8d7fff1545590a81d5bfbb289a5f3f9fb4
z-e-a <e.a.zubkov@yandex.ru>
2024-10-07 16:28:39 +0300

    feat: solve subtask 'Worker Threads'

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,42 @@
+import os from "os";
+import { Worker } from "node:worker_threads";
+
+const WORKER_SOURCE_PATH = "./src/wt/worker.js";
+
 const performCalculations = async () => {
-    // Write your code here
+    const workers = [];
+
+    for (let i = 0; i < os.cpus().length; i++) {
+        workers.push(createWorker(10 + i))
+    }
+
+    Promise.allSettled(workers).then((results) => {
+        console.log(results.map((result) => {
+            if (result.status == "fulfilled") {
+                return {
+                    status: "resolved",
+                    data: result.value.data
+                }
+            } else {
+                return {
+                    status: "error",
+                    data: null
+                }
+            }
+        }));
+    })
 };
+
+function createWorker(workerData) {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(WORKER_SOURCE_PATH, { workerData });
+      worker.on('message', resolve);
+      worker.on('error', reject);
+      worker.on('exit', (code) => {
+        if (code !== 0)
+          reject(new Error(`Worker stopped with exit code ${code}`));
+      })
+    })
+  }
 
 await performCalculations();
